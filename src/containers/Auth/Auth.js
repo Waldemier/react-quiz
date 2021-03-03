5e72ccc9fbd6866db2d56f6a7c13bd752b8a63e8
Wolodymyr <76656624+Waldemier@users.noreply.github.com>
2021-03-03 07:25:13 +0200

    created input component, create auth page from and inputs validation + package is_js for valid mail, start building create quiz page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,139 @@
 import React from 'react'
-
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
+import is from 'is_js'
 export default class Auth extends React.Component {
+
+    state = {
+        isFormValid: false,
+        formControls: {
+            email: {
+                value: '',
+                type: 'email',
+                label: 'Email',
+                errorMessage: 'Enter the correct email',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: '',
+                type: 'password',
+                label: 'Password',
+                errorMessage: 'Enter the correct password',
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength: 6
+                }
+            }
+        }
+       
+    }
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = event => {
+        event.preventDefault(); //Відміняє стандартну поведінку форми
+    }
+
+    validationControl(control, validationParams) {
+
+        if(!validationParams) //На випадок якщо таке поле буде відсутнє в майбутніх input'ах
+        {
+            return true;
+        }
+
+        let isValid = true;
+
+        if(validationParams.required)
+        {
+             isValid = control.value.trim() !== '' && isValid;
+        }
+
+        if(validationParams.email)
+        {
+            isValid = is.email(control.value) && isValid;
+        }
+
+        if(validationParams.minLength)
+        {
+            isValid = control.value.length >= validationParams.minLength && isValid;
+        }
+
+        return isValid;
+
+    }
+
+    onChangeHandler(event, controlName) {  
+        //console.info(`${controlName} ${event.target.value}`)
+        const value = event.target.value
+        const formControls = {...this.state.formControls}
+        const control = { ...formControls[controlName] }
+
+        control.value = value;
+        control.touched = true;
+        control.valid = this.validationControl(control, control.validation);
+
+        formControls[controlName] = control;
+
+        let isFormValid = true;
+
+        Object.keys(formControls).map(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({ 
+            formControls, isFormValid
+        })
+    }
+
+    renderInputs() {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+            return (
+                <Input 
+                    key={controlName + index} //random
+                    value={control.value}
+                    type={control.type}
+                    label={control.label}
+                    errorMessage={control.errorMessage}
+                    valid={control.valid}
+                    shouldValidate={!!control.validation} //Пишемо подвійне заперечення, щоб явно було видно, що передається булеве значення (можливий запис без !!)
+                    touched={control.touched}
+                    onChange={event => this.onChangeHandler(event, controlName)}
+                />
+            )
+        })
+    }
+    
     render() {
+
         return (
-            <div>
-                <h1>Registration page</h1>
+            <div className={classes.Auth}>
+                <div>
+
+                    <h1>Authentication</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        {this.renderInputs()}
+
+                        <Button type="success" onClick={this.loginHandler} disabled={!this.state.isFormValid}>Sign In</Button>
+                        <Button type="primary" onClick={this.registerHandler} disabled={!this.state.isFormValid}>Sign Up</Button>
+                    </form>
+
+                </div>
             </div>
         )
     }
--- src/containers/CreateQuiz/CreateQuiz.js
+++ src/containers/CreateQuiz/CreateQuiz.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import classes from './CreateQuiz.module.css'
+import Button from '../../components/UI/Button/Button'
 export default class CreateQuiz extends React.Component {
+
+    submitHandler = event => {
+        event.preventDefault();
+    }   
+
     render() {
         return (
-            <div>
-                <h1>Create quize page</h1>
+            <div className={classes.CreateQuiz}>
+                <div>
+                    <h1>Create quize page</h1>
+                
+                    <form onSubmit={this.submitHandler}>
+                        <input type="text"/>
+                        <hr/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+
+                        <select></select>
+
+                        <Button type="primary" onClick={this.addQuestionHandler}>Add question</Button>
+                        <Button type="success" onClick={this.createQuizHandler}>Create</Button>
+
+                    </form>
+                </div>
             </div>
         )
     }
